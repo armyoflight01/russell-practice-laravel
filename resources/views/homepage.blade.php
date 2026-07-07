c5579f9ef535d2babcb76d529ab66ae57d3fc401
russell evan O. loquinario <russellevanloquinario@gmail.com>
2026-07-07 15:21:14 +0800

    new updates

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -14,37 +14,111 @@
 
 <body>
 
-    <nav class="m-[80px] flex justify-evenly items-center">
+    <nav class="m-[80px] flex justify-evenly items-center bg-pink-400 p-[20px] rounded-[20px]">
 
-     <h1 class="text-2xl">IT RAFAEL</h1>
+     <h1 class="text-2xl text-white">IT RAFAEL</h1>
 
-     <ul class="flex gap-[20px] text-xl">
-        <li><a href="">Home</a></li>
+     <ul class="flex gap-[20px] text-xl text-white">
+        <li><a id="loginbtn" href="">Home</a></li>
         <li><a href="">Contact</a></li>
      </ul>
 
-  <i class="fa-regular fa-user text-xl"></i>
-  <i class="fa-solid fa-magnifying-glass text-xl"></i>
+  <i class="fa-regular fa-user text-xl text-white"></i>
+  <i class="fa-solid fa-magnifying-glass text-xl text-white"></i>
 
     </nav>
 
     <!---End of the Navbar---> 
+    
 
+    <section class="relative m-[80px] flex items-center justify-center min-h-[400px]">
 
-    <section>
+        <div class="flex flex-col items-center justify-center border border-black p-[20px] gap-[20px] bg-blue-400">
+         
+            <h2 class="text-white">Login Page</h2>
+            <input type="email" placeholder="Enter Email">
+            <input type="password" placeholder="Enter Password">
+            <button class="border border-none p-[10px] text-white bg-pink-300 rounded">Submit</button>
 
-        <div>
-            <img src="{{ asset('images/1.jpg') }}" alt="">
-        </div>
-
-        <div>
-             <img src="{{ asset('images/2.jpg') }}" alt="">
         </div>
 
     </section>
 
+
+     <section class="m-[80px]">
+
+      <div class="grid grid-cols-[repeat(3,_1fr)] gap-6 ">
+
+
+        <div class="flex flex-col items-end justify-start relative nth-[2]:[&>h2]:bg-red-500">
+            <h2 class="border border-none bg-blue-400 p-[20px] rounded-[100%] text-white absolute mt-[10px] mr-[10px]">30 0ff</h2>
+            <img class="w-[100%] h-[40%]"  src="{{ asset('images/1.jpg') }}" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, consectetur nobis repellendus suscipit corporis harum possimus, libero mollitia unde voluptate totam architecto ex. Porro possimus sapiente adipisci? Natus, tempore dolores?</p>
+        </div>
+
+             <div class="flex flex-col items-end justify-start relative">
+            <h2 class="border border-none bg-blue-400 p-[20px] rounded-[100%] text-white absolute mt-[10px] mr-[10px]">30 0ff</h2>
+            <img class="w-[100%] h-[40%]"  src="{{ asset('images/1.jpg') }}" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, consectetur nobis repellendus suscipit corporis harum possimus, libero mollitia unde voluptate totam architecto ex. Porro possimus sapiente adipisci? Natus, tempore dolores?</p>
+        </div>
+
+
+            <div class="flex flex-col items-end justify-start relative">
+            <h2 class="border border-none bg-blue-400 p-[20px] rounded-[100%] text-white absolute mt-[10px] mr-[10px]">30 0ff</h2>
+            <img class="w-[100%] h-[40%]"  src="{{ asset('images/1.jpg') }}" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, consectetur nobis repellendus suscipit corporis harum possimus, libero mollitia unde voluptate totam architecto ex. Porro possimus sapiente adipisci? Natus, tempore dolores?</p>
+        </div>
+
+         <div class="flex flex-col items-end justify-start relative">
+            <h2 class="border border-none bg-blue-400 p-[20px] rounded-[100%] text-white absolute mt-[10px] mr-[10px]">30 0ff</h2>
+            <img class="w-[100%] h-[40%]"  src="{{ asset('images/1.jpg') }}" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, consectetur nobis repellendus suscipit corporis harum possimus, libero mollitia unde voluptate totam architecto ex. Porro possimus sapiente adipisci? Natus, tempore dolores?</p>
+        </div>
+
+           <div class="flex flex-col items-end justify-start relative">
+            <h2 class="border border-none bg-blue-400 p-[20px] rounded-[100%] text-white absolute mt-[10px] mr-[10px]">30 0ff</h2>
+            <img class="w-[100%] h-[40%]"  src="{{ asset('images/1.jpg') }}" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, consectetur nobis repellendus suscipit corporis harum possimus, libero mollitia unde voluptate totam architecto ex. Porro possimus sapiente adipisci? Natus, tempore dolores?</p>
+        </div>
+
+
+              <div class="flex flex-col items-end justify-start relative">
+            <h2 class="border border-none bg-blue-400 p-[20px] rounded-[100%] text-white absolute mt-[10px] mr-[10px]">30 0ff</h2>
+            <img class="w-[100%] h-[40%]"  src="{{ asset('images/1.jpg') }}" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, consectetur nobis repellendus suscipit corporis harum possimus, libero mollitia unde voluptate totam architecto ex. Porro possimus sapiente adipisci? Natus, tempore dolores?</p>
+        </div>
+
+      </div>
+  <!---father end--->
+   
+
+     </section>
+
     <!---END Home landing page--->
 
+
+    <hr class="border border-black">
+    <section class="flex items-center justify-center p-[20px]">
+        <button class="border border-black p-[10px] rounded">GET MORE</button>
+    </section>
+    <hr class="border border-black">
+
+
+    <footer class="m-[80px] flex justify-evenly ">
+        
+        <ul class="text-2xl font-bold flex flex-col gap-[20px]">
+            <li ><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+        
+ 
+       <div class="flex gap-[50px]">
+          <i class="fa-regular fa-user text-2xl"></i>
+         <i class="fa-solid fa-magnifying-glass text-2xl"></i>
+       </div>
+
+    </footer>
 
 
 </body>
